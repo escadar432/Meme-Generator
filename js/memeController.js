e13d52d3d5dfb07c8df8e9d1eab0f8ec53756b84
escadar432 <escadar1610@gmail.com>
2024-08-05 12:15:47 +0300

    Renamed images

--- js/memeController.js
+++ js/memeController.js
@@ -1,1 +1,19 @@
 'use strict'
+
+
+function init() {
+    console.log("Hello project");
+    renderMeme()
+}
+
+function renderMeme() {
+    const canvas = document.getElementById("canvas")
+    const gCtx = canvas.getContext("2d")
+
+    const elImg = new Image()
+    elImg.src = '/css/imgs/meme-imgs-square/1.jpg'
+  
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, elImg.naturalWidth, elImg.naturalHeight)
+    }
+}
--- js/services/memeService.js
+++ js/services/memeService.js
@@ -1,5 +1,7 @@
 'use strict'
+
 var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
@@ -11,4 +13,5 @@ var gMeme = {
         }
     ]
 }
+
 var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
